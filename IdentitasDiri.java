c296399b5624e3349700acbcd45b6954dbcdb7ba
DavidGusdiFernando <akunkuliah.davidfernando@gmail.com>
2023-03-18 20:59:42 +0700

    update kedua

---
+++ IdentitasDiri.java
@@ -0,0 +1,15 @@
+    public class IdentitasDiri {
+        public static void main(String[] args) {
+            String nama = "DAVID GUSDI FERNANDO";
+            String npm = "2022310039";
+            String prodi = "TEKNIK INFORMATIKA";
+            String fakultas = "Fakultas";
+            String universitas = "Nama Universitas";
+    
+            System.out.println("Nama: " + nama);
+            System.out.println("NIM: " + npm);
+            System.out.println("Program Studi: " + prodi);
+            System.out.println("Fakultas: " + fakultas);
+            System.out.println("Universitas: " + universitas);
+        }
+    }
